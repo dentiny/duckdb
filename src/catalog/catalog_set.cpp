26f1593d96bdab6ae912ddc735d2c7f1c3143b5d
dentiny <dentinyhao@gmail.com>
2026-01-12 11:58:30 -0800

    format

--- src/catalog/catalog_set.cpp
+++ src/catalog/catalog_set.cpp
@@ -561,8 +561,9 @@ SimilarCatalogEntry CatalogSet::SimilarEntry(CatalogTransaction transaction, con
 	return result;
 }
 
-optional_ptr<CatalogEntry> CatalogSet::CreateDefaultEntry(CatalogTransaction transaction, const string &name,
-                                                          unique_lock<mutex> &read_lock) DUCKDB_NO_THREAD_SAFETY_ANALYSIS {
+optional_ptr<CatalogEntry>
+CatalogSet::CreateDefaultEntry(CatalogTransaction transaction, const string &name,
+                               unique_lock<mutex> &read_lock) DUCKDB_NO_THREAD_SAFETY_ANALYSIS {
 	// no entry found with this name, check for defaults
 	if (!defaults || defaults->created_all_entries) {
 		// no defaults either: return null
